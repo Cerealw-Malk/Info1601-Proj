42d38c0abc4c148485af8868daff86e8aea5c4a1
Cerealw-Malk <ciel.coryatt@gmail.com>
2025-04-19 21:07:08 -0400

    Fire Base

---
+++ firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCdP9V9PGB-40dDinsCM-UJPJ3cg0FznZY",
+  authDomain: "gymmetry.firebaseapp.com",
+  projectId: "gymmetry",
+  storageBucket: "gymmetry.firebasestorage.app",
+  messagingSenderId: "61726249311",
+  appId: "1:61726249311:web:0740aa3853357571fb66ea",
+  measurementId: "G-859RMBPWR7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export default firebaseConfig;
