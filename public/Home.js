561dd4fedd926ad0e94cf53b48fe905cdb930711
Cerealw-Malk <ciel.coryatt@gmail.com>
2025-04-21 17:18:49 -0400

    Added content to most pages

---
+++ public/Home.js
@@ -0,0 +1,13 @@
+const logo = document.getElementById('logo');
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenuClose = document.getElementById('mobile-menu-close');
+const mobileMenu = document.getElementById('mobile-menu');
+const overlay = document.getElementById('overlay');
+const navLinks = document.querySelectorAll('.nav-link');
+const pages = {
+    'start-workout': document.getElementById('start-workout-page'),
+    'history': document.getElementById('history-page'),
+    'profile': document.getElementById('profile-page'),
+    'exercises': document.getElementById('exercises-page'),
+    'saved-exercises': document.getElementById('saved-exercises-page')
+};
